5d207219d3af630d1e0cf9b5898116aab4013fb5
Shahen Algoo <shahenalgoo@gmail.com>
2023-07-08 01:04:01 +0400

    client passenger informed police

--- app/(client)/components/chat/EmergencyOptions.tsx
+++ app/(client)/components/chat/EmergencyOptions.tsx
@@ -1,22 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { useClientSos } from "@/context/ClientSosContext";
+import { SosReq } from "@/types/typings";
 import { FC } from "react";
 
 import { MdOutlineLocalPolice } from 'react-icons/md';
-import { TbMessage } from "react-icons/tb";
 
 const EmergencyOptions: FC = () => {
+
+    const { sosReq, updateSos } = useClientSos();
+
+    // Update request
+    const handleInformPolice = () => {
+        updateSos(sosReq, { initiator_informed_police: true } as SosReq);
+    }
+
     return (
         <div className="flex justify-between items-center p-2 px-4 bg-black">
             <p className="text-xs text-neutral-400">In the meantime</p>
             <div className="flex gap-2">
-                <Button size='sm' variant='destructive' className="rounded-full bg-rose-700 text-white">
+                <Button onClick={handleInformPolice} size='sm' variant='destructive' className="rounded-full bg-rose-700 text-white">
                     <MdOutlineLocalPolice size={18} className="mr-2" />
                     Call Police
                 </Button>
-                {/* <Button size='sm' variant='destructive' className="rounded-full bg-rose-700 text-white">
-                    <TbMessage size={18} className="mr-2" />
-                    Text 911
-                </Button> */}
             </div>
         </div>
     )
